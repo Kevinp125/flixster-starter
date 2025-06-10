b94e75fc5d83181381c2dd45e12961a1dabbc356
Kevin Pereda <kevinpereda125@meta.com>
2025-06-10 15:32:55 -0700

    Search bar is working in the sense that whenever user types we are saving what they are typing in setSearchInput. Also have the submit button and enter key handlers working good. Now need to make the actual function that searches based off passed in search input.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,11 +4,28 @@ import { useState } from "react";
 
 export default function SearchBar(){
 
+  const [searchInput, setSearchInput] = useState(''); //initialzing state for our input
+
+  //function updates input field everytime user types something
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
+  }
+  
+  //when user hits enter key or clicks the submit button this function will fire which executes the search
+  const handleSubmit = (event) => {
+    if (event.key === 'Enter') {
+      console.log(searchInput);
+    }
+    else{
+      console.log(searchInput);
+    }
+  }
+
   return(
 
     <div>
-      <input type="text"  placeholder="Search..."/>
-      <button>Submit</button>
+      <input type="text" value = {searchInput} onChange = {handleInputChange} onKeyDown = {handleSubmit} placeholder="Search..."/>
+      <button onClick = {handleSubmit}>Submit</button>
       <button>Clear</button>
     </div>
 
