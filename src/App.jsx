50a524c80593b96705d64ef3483903f1a8415bd6
Kevin Pereda <kevinpereda125@meta.com>
2025-06-09 20:24:12 -0700

    Mileston 1.2 I guess you could say. The styles for the movielist container have been applied. Now all the cards are in a grid and the grid columns adjust depending on screen size. Also have a hover effect. Basic layout for how movie cards will be displayed on screen is done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import './stylesheets/movielist.css'
 import MovieList from './components/MovieList'
-
 
 
 const App = () => {
   return (
     <div className="App">
-      <MovieList />
+
+      <section className = "movie-list-container"> {/*Creating a section that holds all the movieCard articles*/} 
+        <MovieList />
+      </section>
+  
     </div>
   )
 }
