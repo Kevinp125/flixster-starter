748339d2c186b02a2f23515f3aa776c87a82eea8
Kevin Pereda <kevinpereda125@meta.com>
2025-06-09 20:43:36 -0700

    Added a quick header and footer for the time being. Made a component and style sheet for each. For the time being the basic layout of the page is done. Moving on to next milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import './stylesheets/movielist.css'
+
 import MovieList from './components/MovieList'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 const App = () => {
   return (
     <div className="App">
+      
+      <Header />
 
       <section className = "movie-list-container"> {/*Creating a section that holds all the movieCard articles*/} 
         <MovieList />
       </section>
+
+      <Footer />
   
     </div>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../stylesheets/header-footer.css'
+
+export default function Footer(){
+
+  return(
+    <footer>
+    </footer>
+  )
+
+}
